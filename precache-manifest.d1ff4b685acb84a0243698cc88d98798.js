58646a2b291adad9ef3ebc03be6b2903bd39a9af
Code Sandbox <hello@codesandbox.io>
2021-11-09 18:27:31 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d1ff4b685acb84a0243698cc88d98798.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ead14c24ec5e6266d617184b16fd4b",
+    "url": "https://bharat549sa.github.io/csb-gufvv/index.html"
+  },
+  {
+    "revision": "d3e1216441ffe7d3ddb9",
+    "url": "https://bharat549sa.github.io/csb-gufvv/static/js/2.66a39e3d.chunk.js"
+  },
+  {
+    "revision": "48ff4e8c7c03277fac37",
+    "url": "https://bharat549sa.github.io/csb-gufvv/static/js/main.926c6f64.chunk.js"
+  },
+  {
+    "revision": "b70313819ac4d889ff04",
+    "url": "https://bharat549sa.github.io/csb-gufvv/static/js/runtime~main.7ab47a01.js"
+  }
+]);
